fcdbdedc71df97d40c0a55af7f268fe228e6148c
Harinirpm <harinirangammal4@gmail.com>
2024-07-13 19:52:55 +0530

    completed

--- Frontend/PS_Events/src/App.jsx
+++ Frontend/PS_Events/src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import EventUploadForm from './assets/Admin/EventUploadForm';
 
 function App() {
   return (
-    <>
-      
-      <h1>Landing Page</h1>
-    </>
-  )
+    <Router>
+      <div className='app'>
+        <h1>Landing Page</h1>
+        <Link to="/upload">
+          <button className='navigate-button'>Create Event</button>
+        </Link>
+
+        <Routes>
+          <Route path="/upload" element={<EventUploadForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
